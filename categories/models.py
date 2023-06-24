ed89d0ba2ea79bd320643227b3713d5c0405c717
Drosos <Drosinakis.drosos1@gmail.com>
2023-06-24 20:20:52 +0300

    add the if i categories

--- categories/models.py
+++ categories/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 
 
 class Category(models.Model):
+    id = models.UUIDField(
+        default=uuid.uuid4(),
+        unique=True,
+        primary_key=True,
+        editable=False,
+    )
     name = models.CharField(max_length=200)
     slug = models.SlugField()
