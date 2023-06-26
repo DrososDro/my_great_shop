5ebe065229565196227a69858caa98e497cbadf2
Drosos Drosinakis <drosinakis.drosos1@gmail.com>
2023-06-26 17:26:07 +0300

    make the model for billing_addres

--- account/models/images_and_billing.py
+++ account/models/images_and_billing.py
@@ -22,3 +22,21 @@ class MultipleImages(models.Model):
 
     def image_url(self):
         return self.image_name.url or ""
+
+
+class BillingAdress(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True,
+    )
+    company = models.CharField(max_length=200, blank=True, null=True)
+    country = models.CharField(blank=True, max_length=20)
+    address_line_1 = models.CharField(blank=True, max_length=100)
+    address_line_2 = models.CharField(blank=True, max_length=100)
+    city = models.CharField(max_length=20)
+    telephone = models.CharField(max_length=200)
+    postcode = models.CharField(max_length=200)
+    comment = models.TextField()
